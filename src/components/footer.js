be97db6184a82490d78ad29f4b9125c7e349eea7
Shoaib_Mohammed <shoaibmohammed7310@gmail.com>
2023-08-05 23:17:37 -0500

    ch commit

--- src/components/footer.js
+++ src/components/footer.js
@@ -102,7 +102,7 @@ const Footer = () => {
       </StyledSocial>
       <StyledMetadata tabindex="-1">
         <StyledGitHubLink
-          href="https://github.com/bchiang7/v4"
+          href="https://github.com/shoaibmohammed7/heuristic.folio"
           target="_blank"
           rel="nofollow noopener noreferrer">
           <div>
