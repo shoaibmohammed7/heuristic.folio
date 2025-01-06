9909d4474c3d7f8aeb3caf85e7aea2ada8c2a66c
Shoaib Mohammed <smohammed7@hawk.iit.edu>
2025-01-05 21:50:09 -0600

    t1

--- src/components/icons/twitter.js
+++ src/components/icons/twitter.js
@@ -1,12 +1,22 @@
 import React from 'react';
 
 const IconTwitter = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 612 612">
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    role="img"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+  >
     <title>Twitter</title>
-    <path
-      d="M714.163 519.284L1160.89 0H1055.03L667.137 450.887L357.328 0H0L468.492 681.821L0 1226.37H105.866L515.491 750.218L842.672 1226.37H1200L714.137 519.284H714.163ZM569.165 687.828L521.697 619.934L144.011 79.6944H306.615L611.412 515.685L658.88 583.579L1055.08 1150.3H892.476L569.165 687.854V687.828Z"
-    />
+    <path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z" />
   </svg>
 );
 
+
 export default IconTwitter;
+
+
